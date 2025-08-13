3f15b181b613785f55f5ae4f2cd5abe803149779
Omar Kassem <omarksm09@gmail.com>
2025-08-13 12:27:23 +0300

    feat: convert newlines to HTML breaks in system announcement emails

--- backend/internal/email_sender.go
+++ backend/internal/email_sender.go
@@ -137,7 +137,7 @@ func (service *MailService) InvoiceMailContent(invoiceTotal float64, currency st
 
 func (service *MailService) SystemAnnouncementMailBody(subject, body string) string {
 	template := string(systemAnnouncementMail)
-
+	body = strings.ReplaceAll(body, "\n", "<br>")
 	template = strings.ReplaceAll(template, "-subject-", subject)
 	template = strings.ReplaceAll(template, "-body-", body)
 
