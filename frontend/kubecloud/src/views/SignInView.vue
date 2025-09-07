da95539e3fa4a1fb7f6534a7bd8ebec4a9d684fc
kassem <omarksm09@gmail.com>
2025-09-07 15:13:54 +0300

    re-branding

--- frontend/kubecloud/src/views/SignInView.vue
+++ frontend/kubecloud/src/views/SignInView.vue
@@ -4,7 +4,7 @@
     <div class="auth-content">
       <div class="auth-header">
         <h1 class="auth-title">Welcome Back!</h1>
-        <p class="auth-subtitle">Sign in to your KubeCloud account</p>
+        <p class="auth-subtitle">Sign in to your Mycelium Cloud account</p>
       </div>
       <v-form @submit.prevent="handleSignIn" class="auth-form" ref="formRef" v-model="isFormValid">
         <v-text-field
