c399438e9cf14bb85cbb13fd9df239ddadce3f83
MohamedElmdary <engm5081@gmail.com>
2025-11-06 12:50:16 +0200

    clean up

--- frontend/kubecloud/public/env.js
+++
@@ -1,6 +0,0 @@
-window.__ENV__ = {
-  VITE_API_BASE_URL: 'http://localhost:8080/api',
-  VITE_NETWORK: 'dev',
-  VITE_STRIPE_PUBLISHABLE_KEY:
-    'pk_test_51Rdp72C4WqB88qsawZgMYCdQo9t6AMBJktvBAwvca8qYHwDobDihxmesYs3oisucfGmk7n0FmLBPdizSTCjlwPcu005CEA2vQ8',
-}
