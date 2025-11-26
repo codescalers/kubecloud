1497ddac6fb1896a6e0bfb71549bb719ed764761
MohamedElmdary <engm5081@gmail.com>
2025-11-26 14:33:33 +0200

    Add util to check if task completed or not

--- frontend/kubecloud/src/utils/userService.ts
+++ frontend/kubecloud/src/utils/userService.ts
@@ -311,6 +311,20 @@ export class UserService {
     return response.data.data.pending_records
   }
 
+  async waitTaskTocomplete(id: string): Promise<boolean> {
+    const res = await api.get<ApiResponse<"completed" | "failed">>(`/v1/workflow/${id}`)
+    if (res.data.data === "failed") {
+      return false
+    }
+
+    if (res.data.data === "completed") {
+      return true
+    }
+
+    await new Promise(res => setTimeout(res, 5000))
+    return this.waitTaskTocomplete(id)
+  }
+  
   // Fetch twin account info
   async getTwinAccount(twinId: number): Promise<TwinResponse> {
     const response = await api.get<ApiResponse<TwinResponse>>(`/v1/twins/${twinId}/account`, {
