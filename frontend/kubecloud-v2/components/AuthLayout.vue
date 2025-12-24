4e6ef9aed9c417bc155810fb376fbd679c82dee3
MohamedElmdary <engm5081@gmail.com>
2025-12-25 00:37:51 +0200

    Finalized authLayout

--- frontend/kubecloud-v2/components/AuthLayout.vue
+++ frontend/kubecloud-v2/components/AuthLayout.vue
@@ -1,35 +1,24 @@
 <template>
   <v-row no-gutters class="h-100">
-    <!-- class="px-10 bg-surface" -->
-    <v-col
-      cols="12"
-      md="6"
-      :style="{
-        maxHeight: '100%',
-        overflow: 'hidden',
-      }"
-    >
+    <v-col class="mh-100 overflow-hidden" cols="12" md="6">
       <div class="h-100 d-flex align-center">
         <slot />
       </div>
     </v-col>
 
-    <v-col cols="6" class="d-none d-md-block">
+    <v-col cols="6" class="d-none d-md-block" :order="reversed ? 'first' : 'last'">
       <div
         class="h-100 w-100 bg-primary d-flex justify-center align-center px-10"
         :style="{
-          background: `url(${myceliumBg1}) no-repeat bottom right/70%, linear-gradient(180deg, #182F66 0%, #0F172A 100%)`,
+          background: `url(${reversed ? myceliumBg2 : myceliumBg1}) no-repeat  ${reversed ? '-250px 170px' : 'bottom right'}/70%, linear-gradient(180deg, #182F66 0%, #0F172A 100%)`,
         }"
       >
         <div :style="{ maxWidth: '560px' }">
-          <h2 :style="{ fontSize: '32px', fontWeight: '700' }">Join Mycelium Cloud</h2>
-          <div :style="{ fontSize: '20px' }">
-            <p class="mt-3 mb-8">Start your journey into decentralized cloud infrastructure.</p>
+          <h2 class="text-h4 font-weight-bold" v-text="title" />
+          <div class="text-h6">
+            <p class="mt-3 mb-8" v-text="quote" />
 
-            <p>
-              Create your account to access secure compute, scalable resources, and a
-              next-generation cloud built without central points of failure.
-            </p>
+            <p v-text="subtitle" />
           </div>
         </div>
       </div>
@@ -38,10 +27,13 @@
 </template>
 
 <script setup lang="ts">
-// import { useDisplay } from "vuetify"
 import myceliumBg1 from "~/assets/images/mycelium_bg_1.svg"
+import myceliumBg2 from "~/assets/images/mycelium_bg_2.svg"
 
-defineProps<{ cols: number; prependWorkart?: boolean; hideWorkart?: boolean }>()
-
-// const { smAndDown } = useDisplay()
+defineProps<{
+  title: string
+  quote: string
+  subtitle: string
+  reversed?: boolean
+}>()
 </script>
